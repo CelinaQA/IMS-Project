eeacf41fd3c575fc31d8a11a22010c51fd98713a
Celina <cbasa@qa.com>
2021-01-11 17:23:12 +0000

    made edits to file

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,2 +1,29 @@
-create database if not exists ims;
-create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+CREATE DATABASE IF NOT EXISTS ims;
+CREATE TABLE IF NOT EXISTS ims.customers (
+customer_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100) NOT NULL,
+PRIMARY KEY (customer_id)
+);
+CREATE TABLE IF NOT EXISTS ims.orders (
+order_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+customer_id INT NOT NULL,
+date_placed DATETIME DEFAULT NOW() NOT NULL,
+PRIMARY KEY (order_id),
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+CREATE TABLE IF NOT EXISTS ims.items (
+item_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+item_name VARCHAR(100) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock INT NOT NULL,
+PRIMARY KEY (item_id)
+);
+CREATE TABLE IF NOT EXISTS ims.orderlines (
+orderline_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+order_id INT NOT NULL,
+item_id INT NOT NULL,
+PRIMARY KEY (orderline_id),
+FOREIGN KEY (order_id) REFERENCES orders(order_id),
+FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
