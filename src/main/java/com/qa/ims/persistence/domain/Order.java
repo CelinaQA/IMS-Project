27f9affb1eabbea98bf16cc76fd5d5ab19213da3
Celina <cbasa@qa.com>
2021-01-13 15:00:02 +0000

    Added toString, hashCode and equals method to Order and began adding OrderController

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -50,4 +50,47 @@ public class Order {
 	public void setDate_placed(Date date_placed) {
 		this.date_placed = date_placed;
 	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", customer_id=" + customer_id + ", date_placed=" + date_placed + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customer_id == null) ? 0 : customer_id.hashCode());
+		result = prime * result + ((date_placed == null) ? 0 : date_placed.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (customer_id == null) {
+			if (other.customer_id != null)
+				return false;
+		} else if (!customer_id.equals(other.customer_id))
+			return false;
+		if (date_placed == null) {
+			if (other.date_placed != null)
+				return false;
+		} else if (!date_placed.equals(other.date_placed))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
