7069732661d2e6eae31b9ba9721a268c01855713
Celina <cbasa@qa.com>
2021-01-13 15:11:14 +0000

    Added methods for OrderController

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,5 +1,61 @@
 package com.qa.ims.controller;
 
-public class OrderController {
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.services.CrudServices;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private CrudServices<Order> orderService;
+	
+	public OrderController(CrudServices<Order> orderService) {
+		this.orderService = orderService;
+	}
+	
+	String getInput() {
+		return Utils.getInstance().getInput();
+	}
+
+	@Override
+	public List<Order> readAll() {
+		List<Order> orders = orderService.readAll();
+		for (Order order : orders) {
+			LOGGER.info(order.toString());
+		}
+		return orders;
+	}
+
+	@Override
+	public Order create() {
+		LOGGER.info("Please enter a customer ID");
+		Long customer_id = Long.valueOf(getInput());
+		Order order = orderService.create(new Order(customer_id));
+		LOGGER.info("Order created");
+		return order;
+	}
+
+	@Override
+	public Order update() {
+		LOGGER.info("Please enter the id of the order you would like to update");
+		Long id = Long.valueOf(getInput());
+		LOGGER.info("Please enter a customer ID");
+		Long customer_id = Long.valueOf(getInput());
+		Order order = orderService.update(new Order(id, customer_id));
+		LOGGER.info("Order Updated");
+		return order;
+	}
+
+	@Override
+	public void delete() {
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long id = Long.valueOf(getInput());
+		orderService.delete(id);
+	}
 }
