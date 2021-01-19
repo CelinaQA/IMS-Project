8dd59f59c47a8ef9e794b8d4f08b7fbee4734e93
Celina <cbasa@qa.com>
2021-01-19 12:42:15 +0000

    Order controller readAll method test pass

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -1,5 +1,7 @@
 package com.qa.ims;
 
+import java.util.Calendar;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,8 +10,9 @@ public class Runner {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static void main(String[] args) {
-		IMS ims = new IMS();
-		ims.imsSystem();
+//		IMS ims = new IMS();
+//		ims.imsSystem();
+		System.out.println(Calendar.getInstance().getTime());
 	}
 
 }
--- src/test/java/com/qa/ims/controller/OrderControllerTest.java
+++ src/test/java/com/qa/ims/controller/OrderControllerTest.java
@@ -3,17 +3,21 @@ package com.qa.ims.controller;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.services.OrderServices;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OrderControllerTest {
 	
 	@Mock
@@ -27,9 +31,9 @@ public class OrderControllerTest {
 	public void readAllTest() {
 		OrderController orderController = new OrderController(orderServices);
 		List<Order> orders = new ArrayList<>();
-		orders.add(new Order(1L, 1L, 1));
-		orders.add(new Order(2L, 2L, 2));
-		orders.add(new Order(3L, 3L, 3));
+		orders.add(new Order(100L, 1L, Calendar.getInstance().getTime(), 1.50f, 1L, 5));
+		orders.add(new Order(101L, 2L, Calendar.getInstance().getTime(), 2.00f, 2L, 10));
+		orders.add(new Order(102L, 3L, Calendar.getInstance().getTime(), 2.50f, 3L, 15));
 		Mockito.when(orderServices.readAll()).thenReturn(orders);
 		assertEquals(orders, orderController.readAll());
 	}
@@ -41,20 +45,23 @@ public class OrderControllerTest {
 		String quantity = "3";
 		Mockito.doReturn(customer_id,item_id,quantity).when(orderController).getInput();
 		Order order = new Order(1L, 1L, 3);
-		Order savedOrder = new Order(1L, 1L, 1L, 3);
+		Order savedOrder = new Order(100L, 1L, Calendar.getInstance().getTime(), 1.00f, 1L, 3);
 		Mockito.when(orderServices.create(order)).thenReturn(savedOrder);
-		assertEquals(savedOrder, orderController.create());
+		assertEquals(savedOrder, orderController.create()); 
 	}
 	
 	@Test
-	public void updateTest() {
-		String order_id = "1";
+	public void updateAddTest() {
+		String order_id = "100";
 		String customer_id = "1";
 		String item_id = "1";
 		String quantity = "3";
-		Mockito.doReturn(order_id,customer_id,item_id,quantity).when(orderController).getInput();
+		String option = "a";
+		
+		Mockito.doReturn(customer_id,order_id,item_id,quantity).when(orderController).getInput();
+		Mockito.doReturn(option).when(mock);
 		Order order = new Order(1L, 1L, 1L, 3);
-		Mockito.when(orderServices.updateAddItem(order, item_id, item_quantity)).thenReturn(order);
+		Mockito.when(orderServices.updateAddItem(order, 1L, 3)).thenReturn(new Order(100L, 1L, Calendar.getInstance().getTime(), 1.00f, 1L, 3));
 		assertEquals(order, orderController.update());
 	}
 
