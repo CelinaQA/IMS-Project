84e20c3f3a6dd7328c2ef0c03d4bf0c843f30274
Celina <cbasa@qa.com>
2021-01-19 19:54:59 +0000

    Started CustomerDaoMysql test

--- src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -1,5 +1,12 @@
 package com.qa.ims.persistence.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CustomerDaoMysqlTest {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	
 
 }
