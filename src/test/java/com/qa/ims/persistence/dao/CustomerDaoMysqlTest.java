0080c18e5803c3e9835e71d7d1e7db20bca74ffe
Celina <cbasa@qa.com>
2021-01-20 13:31:27 +0000

    CustomerDaoMysql readAll method test passed

--- src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -48,11 +50,18 @@ public class CustomerDaoMysqlTest {
 	
 	@Before
 	public void setup() {
-		try (Connection connection = DBUtils.getInstance().getConnection();
-				Statement statement = connection.createStatement();) {
-			statement.executeUpdate("DELETE FROM orderlines; DELETE FROM items; DELETE FROM orders; DELETE FROM customers;");
-		} catch (Exception e) {
-			LOGGER.debug(e.getStackTrace());
+		try (Connection connection = DBUtils.getInstance(DB_USER, DB_PASS).getConnection();
+				BufferedReader br = new BufferedReader(new FileReader(DB_FILE));) {
+			String string;
+			while ((string = br.readLine()) != null) {
+				try (Statement statement = connection.createStatement();) {
+					statement.executeUpdate(string);
+				}
+			}
+		} catch (SQLException | IOException e) {
+			for (StackTraceElement ele : e.getStackTrace()) {
+				LOGGER.debug(ele);
+			}
 			LOGGER.error(e.getMessage());
 		}
 	}
@@ -67,5 +76,18 @@ public class CustomerDaoMysqlTest {
 		customer = customerDaoMysql.create(customer);
 		assertEquals(savedCustomer, customer);
 	}
+	
+	@Test
+	public void readAllTest() {
+		CustomerDaoMysql customerDaoMysql = new CustomerDaoMysql();
+		List<Customer> customers = new ArrayList<>();
+		Customer customer1 = new Customer(1L,"first", "person");
+		Customer customer2 = new Customer(2L,"second", "person");
+		customers.add(customer1);
+		customers.add(customer2);
+		customerDaoMysql.create(new Customer("first", "person"));
+		customerDaoMysql.create(new Customer("second", "person"));
+		assertEquals(customers, customerDaoMysql.readAll());
+	} 
 	
 }
