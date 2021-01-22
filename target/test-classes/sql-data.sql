41b27458da352d77a6c2e66bda8742d11f9a9f26
Celina <cbasa@qa.com>
2021-01-22 14:36:47 +0000

    Added target folder to root

---
+++ target/test-classes/sql-data.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS ims;
+DROP TABLE IF EXISTS ims.orderlines;
+DROP TABLE IF EXISTS ims.items;
+DROP TABLE IF EXISTS ims.orders;
+DROP TABLE IF EXISTS ims.customers;
+CREATE TABLE IF NOT EXISTS ims.customers(customer_id INT UNIQUE NOT NULL AUTO_INCREMENT, first_name VARCHAR(100) NOT NULL, last_name VARCHAR(100) NOT NULL, PRIMARY KEY (customer_id));
+CREATE TABLE IF NOT EXISTS ims.orders(order_id INT UNIQUE NOT NULL AUTO_INCREMENT, customer_id INT NOT NULL, date_placed DATETIME DEFAULT NOW() NOT NULL, PRIMARY KEY (order_id), FOREIGN KEY (customer_id) REFERENCES customers(customer_id)) AUTO_INCREMENT=100;
+CREATE TABLE IF NOT EXISTS ims.items(item_id INT UNIQUE NOT NULL AUTO_INCREMENT, item_name VARCHAR(100) NOT NULL, price DECIMAL(10,2) NOT NULL, stock INT NOT NULL, PRIMARY KEY (item_id));
+CREATE TABLE IF NOT EXISTS ims.orderlines(orderline_id INT UNIQUE NOT NULL AUTO_INCREMENT, order_id INT NOT NULL, item_id INT NOT NULL, quantity INT NOT NULL,total_price DECIMAL(10,2) NOT NULL, PRIMARY KEY (orderline_id), FOREIGN KEY (order_id) REFERENCES orders(order_id), FOREIGN KEY (item_id) REFERENCES items(item_id));
+INSERT INTO ims.items(item_name,price,stock) VALUES('bread',1.00,100);
+INSERT INTO ims.items(item_name,price,stock) VALUES('cookies',1.00,100);
+INSERT INTO ims.customers(first_name,last_name) VALUES('first','person');
+INSERT INTO ims.customers(first_name,last_name) VALUES('second','person');
